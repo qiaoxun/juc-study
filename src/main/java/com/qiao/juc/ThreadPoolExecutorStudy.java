c117fa39c14dea1b753191d1d297b354798e3bf3
Xun Qiao <woqiaoxun@163.com>
2021-12-14 22:12:01 +0800

    first commit

---
+++ src/main/java/com/qiao/juc/ThreadPoolExecutorStudy.java
@@ -0,0 +1,14 @@
+package com.qiao.juc;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolExecutorStudy {
+
+    public static void main(String[] args) {
+        ExecutorService executors = Executors.newFixedThreadPool(1);
+        executors.submit()
+    }
+
+}
+
